0a1b8a7959f36ba196f5b4e9917ce734690bf8a7
Ogochukwu Ozotta <88560609+ogochukwu-ozotta@users.noreply.github.com>
2023-12-25 18:16:11 -0600

    class instant

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+import pandas as pd
+
+df = pd.read_csv("hotels.csv")
+
+
 class Hotel:
+    def __init__(self, id):
+        pass
+
     def book(self):
+        pass
+
+    def available(self):
         pass
 
 
 class ReservationTicket:
+    def __init__(self, name, hotel):
     def generate(self):
         pass
+
+
+print(df)
+id = input("Enter the id of the hotel: ")
+hotel = Hotel(id)
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name")
+    reservation_ticket = ReservationTicket(name, hotel)
